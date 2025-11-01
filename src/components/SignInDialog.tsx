55065f2f227d4082a1d5dd03584e38840958a984
AmirtterK <strikereureka7661@gmail.com>
2025-11-01 12:07:06 +0100

    final?

--- src/components/SignInDialog.tsx
+++ src/components/SignInDialog.tsx
@@ -52,129 +52,127 @@ export default function SignInDialog({
   }
 
   return (
-    <section className="flex min-h-screen bg-zinc-100 px-4 py-16 md:py-32 dark:bg-transparent">
-      <form
-        onSubmit={handleSubmit}
-        className="bg-muted m-auto h-fit w-full max-w-sm overflow-hidden rounded-[calc(var(--radius)+.125rem)] border shadow-md shadow-zinc-950/5 dark:[--color-muted:var(--color-zinc-900)]"
-      >
-        <div className="bg-background -m-px rounded-[calc(var(--radius)+.125rem)] border p-8 pb-6">
-          <div className="text-center">
-            <Link href="/" aria-label="go home" className="mx-auto block w-fit">
-              <img src="/logo.png" alt="" className="size-10" />
-            </Link>
-            <h1 className="mb-1 mt-4 text-xl font-semibold">
-              Sign In to Handla
-            </h1>
-            <p className="text-sm">Welcome back! Sign in to continue</p>
+    <form
+      onSubmit={handleSubmit}
+      className="bg-muted w-full max-w-sm overflow-hidden rounded-[calc(var(--radius)+.125rem)] border shadow-md shadow-zinc-950/5 dark:[--color-muted:var(--color-zinc-900)]"
+    >
+      <div className="bg-background -m-px rounded-[calc(var(--radius)+.125rem)] border p-8 pb-6">
+        <div className="text-center">
+          <Link href="/" aria-label="go home" className="mx-auto block w-fit">
+            <img src="/logo.png" alt="" className="size-10" />
+          </Link>
+          <h1 className="mb-1 mt-4 text-xl font-semibold">
+            Sign In to Handla
+          </h1>
+          <p className="text-sm">Welcome back! Sign in to continue</p>
+        </div>
+
+        <div className="mt-6 space-y-6">
+          <div className="space-y-2">
+            <Label htmlFor="email" className="block text-sm">
+              Email
+            </Label>
+            <Input
+              type="email"
+              required
+              name="email"
+              id="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
           </div>
 
-          <div className="mt-6 space-y-6">
-            <div className="space-y-2">
-              <Label htmlFor="email" className="block text-sm">
-                Email
+          <div className="space-y-0.5">
+            <div className="flex items-center justify-between">
+              <Label htmlFor="pwd" className="text-sm">
+                Password
               </Label>
-              <Input
-                type="email"
-                required
-                name="email"
-                id="email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-              />
+              <Button asChild variant="link" size="sm">
+                <Link href="#" className="text-sm">
+                  Forgot your Password?
+                </Link>
+              </Button>
             </div>
-
-            <div className="space-y-0.5">
-              <div className="flex items-center justify-between">
-                <Label htmlFor="pwd" className="text-sm">
-                  Password
-                </Label>
-                <Button asChild variant="link" size="sm">
-                  <Link href="#" className="text-sm">
-                    Forgot your Password?
-                  </Link>
-                </Button>
-              </div>
-              <Input
-                type="password"
-                required
-                name="pwd"
-                id="pwd"
-                value={pwd}
-                onChange={(e) => setPwd(e.target.value)}
-              />
-            </div>
-
-            {error && <p className="text-red-500 text-sm">{error}</p>}
-
-            <Button className="w-full cursor-pointer" type="submit">
-              Sign In
-            </Button>
+            <Input
+              type="password"
+              required
+              name="pwd"
+              id="pwd"
+              value={pwd}
+              onChange={(e) => setPwd(e.target.value)}
+            />
           </div>
 
-          <div className="my-6 grid grid-cols-[1fr_auto_1fr] items-center gap-3">
-            <hr className="border-dashed" />
-            <span className="text-muted-foreground text-xs">
-              Or continue with
-            </span>
-            <hr className="border-dashed" />
-          </div>
+          {error && <p className="text-red-500 text-sm">{error}</p>}
 
-          <div className="grid grid-cols-1">
-            <Button
-              className="cursor-pointer"
-              type="button"
-              variant="outline"
-              onClick={() => {
-                if (!isLoaded) return;
-                signIn.authenticateWithRedirect({
-                  strategy: "oauth_google",
-                  redirectUrl: window.location.pathname,
-                  redirectUrlComplete: "/",
-                });
-              }}
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="0.98em"
-                height="1em"
-                viewBox="0 0 256 262"
-              >
-                <path
-                  fill="#4285f4"
-                  d="M255.878 133.451c0-10.734-.871-18.567-2.756-26.69H130.55v48.448h71.947c-1.45 12.04-9.283 30.172-26.69 42.356l-.244 1.622l38.755 30.023l2.685.268c24.659-22.774 38.875-56.282 38.875-96.027"
-                />
-                <path
-                  fill="#34a853"
-                  d="M130.55 261.1c35.248 0 64.839-11.605 86.453-31.622l-41.196-31.913c-11.024 7.688-25.82 13.055-45.257 13.055c-34.523 0-63.824-22.773-74.269-54.25l-1.531.13l-40.298 31.187l-.527 1.465C35.393 231.798 79.49 261.1 130.55 261.1"
-                />
-                <path
-                  fill="#fbbc05"
-                  d="M56.281 156.37c-2.756-8.123-4.351-16.827-4.351-25.82c0-8.994 1.595-17.697 4.206-25.82l-.073-1.73L15.26 71.312l-1.335.635C5.077 89.644 0 109.517 0 130.55s5.077 40.905 13.925 58.602z"
-                />
-                <path
-                  fill="#eb4335"
-                  d="M130.55 50.479c24.514 0 41.05 10.589 50.479 19.438l36.844-35.974C195.245 12.91 165.798 0 130.55 0C79.49 0 35.393 29.301 13.925 71.947l42.211 32.783c10.59-31.477 39.891-54.251 74.414-54.251"
-                />
-              </svg>
-              <span>Google</span>
-            </Button>
-          </div>
+          <Button className="w-full cursor-pointer" type="submit">
+            Sign In
+          </Button>
         </div>
 
-        <div className="p-3">
-          <p className="text-accent-foreground text-center text-sm">
-            Don&apos;t have an account?
-            <Button
-              variant="link"
-              className="px-2 cursor-pointer"
-              type="button"
-              onClick={onSwitchToSignUp}
-            >
-              Create account
-            </Button>
-          </p>
+        <div className="my-6 grid grid-cols-[1fr_auto_1fr] items-center gap-3">
+          <hr className="border-dashed" />
+          <span className="text-muted-foreground text-xs">
+            Or continue with
+          </span>
+          <hr className="border-dashed" />
         </div>
-      </form>
-    </section>
+
+        <div className="grid grid-cols-1">
+          <Button
+            className="cursor-pointer"
+            type="button"
+            variant="outline"
+            onClick={() => {
+              if (!isLoaded) return;
+              signIn.authenticateWithRedirect({
+                strategy: "oauth_google",
+                redirectUrl: window.location.pathname,
+                redirectUrlComplete: "/",
+              });
+            }}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="0.98em"
+              height="1em"
+              viewBox="0 0 256 262"
+            >
+              <path
+                fill="#4285f4"
+                d="M255.878 133.451c0-10.734-.871-18.567-2.756-26.69H130.55v48.448h71.947c-1.45 12.04-9.283 30.172-26.69 42.356l-.244 1.622l38.755 30.023l2.685.268c24.659-22.774 38.875-56.282 38.875-96.027"
+              />
+              <path
+                fill="#34a853"
+                d="M130.55 261.1c35.248 0 64.839-11.605 86.453-31.622l-41.196-31.913c-11.024 7.688-25.82 13.055-45.257 13.055c-34.523 0-63.824-22.773-74.269-54.25l-1.531.13l-40.298 31.187l-.527 1.465C35.393 231.798 79.49 261.1 130.55 261.1"
+              />
+              <path
+                fill="#fbbc05"
+                d="M56.281 156.37c-2.756-8.123-4.351-16.827-4.351-25.82c0-8.994 1.595-17.697 4.206-25.82l-.073-1.73L15.26 71.312l-1.335.635C5.077 89.644 0 109.517 0 130.55s5.077 40.905 13.925 58.602z"
+              />
+              <path
+                fill="#eb4335"
+                d="M130.55 50.479c24.514 0 41.05 10.589 50.479 19.438l36.844-35.974C195.245 12.91 165.798 0 130.55 0C79.49 0 35.393 29.301 13.925 71.947l42.211 32.783c10.59-31.477 39.891-54.251 74.414-54.251"
+              />
+            </svg>
+            <span>Google</span>
+          </Button>
+        </div>
+      </div>
+
+      <div className="p-3">
+        <p className="text-accent-foreground text-center text-sm">
+          Don&apos;t have an account?
+          <Button
+            variant="link"
+            className="px-2 cursor-pointer"
+            type="button"
+            onClick={onSwitchToSignUp}
+          >
+            Create account
+          </Button>
+        </p>
+      </div>
+    </form>
   );
 }
