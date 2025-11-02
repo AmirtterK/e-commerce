058162f7a03851e976192ee5e585fa5572dc65a4
AmirtterK <strikereureka7661@gmail.com>
2025-11-02 08:25:49 +0100

    typo fix

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -4,7 +4,6 @@ import SlideShow from "./SlideShow";
 export default function HeroSection() {
   return (
     <div className="overflow-hidden">
-  
       <section className="relative pt-24 md:pt-36 px-5">
         <div className="max-w-6xl mx-auto">
           <SlideShow />
